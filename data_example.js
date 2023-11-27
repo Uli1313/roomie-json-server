134fad433aed00b8776936e560740d55e4ad3bd1
Peng <linken0508@gmail.com>
2023-11-27 16:33:32 +0800

    json資料rents格式

---
+++ data_example.js
@@ -0,0 +1,49 @@
+let data = {
+    "id": 1,
+    "userId": 1,
+    "status": "已媒合", // 已媒合、未媒合
+    "updateDate": "2023/09/28 22:08",
+    "view": 456,
+    "address": "新竹市",
+    // 臺北、新北、基隆、桃園、竹市、竹縣、臺中、彰化、苗栗、南投、雲林、高雄、臺南、嘉市、嘉縣、屏東、宜蘭、花蓮、臺東、澎湖、金門、連江
+    "district": ["東區", "埔頂路一段"], // 新增行政區
+    "floor": 8,
+    "totalFloor": 21,
+    "type": "獨立套房", 
+    // 整層住家、獨立套房、分租套房、雅房、其他
+    "title": "新竹市科學園區套房找室友",
+    "square Footage": 15,
+    "houseLayout": "2房1廳1衛",
+    "equipment": [
+      "床",
+      "電視",
+      "衣櫃",
+      "桌子",
+      "椅子",
+      "網路",
+      "冷氣",
+      "洗衣機"
+    ],
+    // 網路、冰箱、電視、冷氣、洗衣機、椅子、桌子、衣櫃、床
+    "price": 21000,
+    "deposit": "一個月",
+    "priceInclude": ["水費", "網路費"],
+    "manageFee": 777,
+    "minPeriod": "一年",
+    "needPartner": 1,
+    "identity": ["學生", "上班族"],
+    // 學生、家庭、上班族
+    "gender": "男性",
+    // 皆可、男性、女性
+    "canPet": true,
+    "canCooking": false,
+    "minAge": 18,
+    "maxAge": 40,
+    "moveInDate": "2023/11/9",
+    "lifeEquipment": ["學校", "夜市"],
+    // 學校、商圈、捷運站、醫療機構
+    "traffic": ["新竹火車站"],
+    "intro": "...",
+    "photo": ["...", "...", "...","...", "...", "..."], // 6張
+    "agreePublish": true
+}
